ea9e70f0ad2afb5d0b9cd3451dfe294b48f09f92
SaulRiglez <saul.riglez@gmail.com>
2016-12-30 12:58:49 -0500

    commiting: description and project

--- app/src/main/java/com/example/victo/salarymanagement/HomePage.java
+++ app/src/main/java/com/example/victo/salarymanagement/HomePage.java
@@ -1,15 +1,13 @@
 package com.example.victo.salarymanagement;
 /*
+Project name:  Salary Management
+Description: Apps to manage the employment salary according to the total hours worked.
 Víctor Branch 12/30/2016
  */
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-/*
-Project name:  Salary Management
-Description: Apps to manage the employment salary according to the total hours worked.
- */
 
 public class HomePage extends AppCompatActivity {
 
