70c7195c91baf09ab524d9c0963e0d6abb0d13d1
Victor Isai Peña <Victor Isai Pea>
2017-01-01 19:41:53 -0500

    Authentication changes

--- app/src/main/java/com/example/victo/salarymanagement/HomePage.java
+++ app/src/main/java/com/example/victo/salarymanagement/HomePage.java
@@ -6,8 +6,10 @@ Developers: Victor , Saul , Ramesh */
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,25 +19,45 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class HomePage extends AppCompatActivity {
+    //Constants
     private static final String TAG = "Authentication" ;
-    //Attributes
+
+    //Authentication attributes
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
-    TextView tvUserName,tvPassword;
-    EditText etUserName, etPassword;
+    private FirebaseUser user;
+
+    //Layout attributes
+    TextView tvUserName,tvPassword,tvRegisterName,tvRegisterEmail,tvRegisterPassword,tvBusinessRole;
+    EditText etUserName, etPassword,etRegisterName,etRegisterEmail,etRegisterPassword;
+    CheckBox chbManager,chbEmployee;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
         //Layout Elements
         tvUserName = (TextView) findViewById(R.id.tvUserName);
         tvPassword = (TextView) findViewById(R.id.tvPassword);
+        tvRegisterName = (TextView) findViewById(R.id.tvName);
+        tvRegisterEmail = (TextView) findViewById(R.id.tvRegisterEmail);
+        tvRegisterPassword = (TextView) findViewById(R.id.tvRegisterPassword);
+        tvBusinessRole = (TextView) findViewById(R.id.tvRegisterBusinessRole);
         etUserName = (EditText) findViewById(R.id.eTuserName);
         etPassword = (EditText) findViewById(R.id.eTpassword);
+        etRegisterName = (EditText) findViewById(R.id.eTName);
+        etRegisterEmail = (EditText) findViewById(R.id.eTregisterEmail);
+        etRegisterPassword = (EditText) findViewById(R.id.eTRegisterPassword);
+        chbManager = (CheckBox) findViewById(R.id.chbManager);
+        chbEmployee = (CheckBox) findViewById(R.id.chbEmployee);
+
         //Authentication
         mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
@@ -43,6 +65,13 @@ public class HomePage extends AppCompatActivity {
                 if (user != null) {
                     // User is signed in
                     Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                    //Sign in is succeed
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    String uid = user.getUid();
+
+                    Toast.makeText(HomePage.this, "Succesfully signed in by: "+email, Toast.LENGTH_SHORT).show();
+
                 } else {
                     // User is signed out
                     Log.d(TAG, "onAuthStateChanged:signed_out");
@@ -63,26 +92,163 @@ public class HomePage extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         if (mAuthListener != null) {
+            signOut();
             mAuth.removeAuthStateListener(mAuthListener);
         }
     }
 
     public void logIn(View view) {
-        mAuth.signInWithEmailAndPassword(etUserName.getText().toString(),etPassword.getText().toString())
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+        String email = etUserName.getText().toString();
+        String password = etPassword.getText().toString();
+        signIn(email,password);
+    }
 
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail", task.getException());
-                            Toast.makeText(HomePage.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+    public void registerUser(View view) {
+        String email = etRegisterEmail.getText().toString();
+        String password = etRegisterPassword.getText().toString();
+        String name = etRegisterName.getText().toString();
+        if(checkBusinessRole()){
+            if(chbEmployee.isChecked()){
+                createAccount(name,email,password,"employee");
+            } else {
+                createAccount(name,email,password,"manager");
+            }
+        }
+
+    }
+
+    private void signIn(String email,String password){
+
+        if(validateFormLogIn()){
+            mAuth.signInWithEmailAndPassword(etUserName.getText().toString(),etPassword.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                            clearWindowLogIn();
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Log.w(TAG, "signInWithEmail", task.getException());
+                                Toast.makeText(HomePage.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
                         }
-                    }
-                });
+                    });
+
+        }
+    }
+
+    private void signOut(){
+        mAuth.signOut();
+    }
+
+    private boolean checkBusinessRole(){
+        boolean valid = true;
+        if(!chbEmployee.isChecked() && !chbManager.isChecked()){
+            Toast.makeText(this, "You have to select your business role", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else {
+            chbEmployee.setError(null);
+            chbManager.setError(null);
+        }
+
+        if(chbManager.isChecked() && chbEmployee.isChecked()){
+            Toast.makeText(this, "You can not have both business roles", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else {
+            chbEmployee.setError(null);
+            chbManager.setError(null);
+        }
+        return valid;
+    }
+
+    private boolean validateFormLogIn(){
+        boolean valid = true;
+        String email = etUserName.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            etUserName.setError("Required");
+            valid = false;
+        } else {
+            etUserName.setError(null);
+        }
+
+        if(TextUtils.isEmpty(password)){
+            etPassword.setError("Required");
+            valid = false;
+        } else {
+            etPassword.setError(null);
+        }
+
+        return valid;
+    }
+
+    private boolean validateFormRegister(){
+        boolean valid = true;
+        String email = etRegisterEmail.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            etRegisterEmail.setError("Required");
+            valid = false;
+        } else {
+            etRegisterEmail.setError(null);
+        }
+
+        String password = etRegisterPassword.getText().toString();
+        if(TextUtils.isEmpty(password)){
+            etRegisterPassword.setError("Required");
+            valid = false;
+        } else {
+            etRegisterPassword.setError(null);
+        }
+
+        String name = etRegisterName.getText().toString();
+        if(TextUtils.isEmpty(name)){
+            etRegisterName.setError("Required");
+            valid = false;
+        } else {
+            etRegisterName.setError(null);
+        }
+        return valid;
+    }
+
+    private void createAccount(String name, String email, String password, String businessRole){
+        if(validateFormRegister()){
+            mAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                            clearWindowRegister();
+                            // If sign in fails, display a message to the user. If sign in succeeds
+                            // the auth state listener will be notified and logic to handle the
+                            // signed in user can be handled in the listener.
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(HomePage.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+
+    }
+    public void logOut(View view) {
+        signOut();
+    }
+
+    private void clearWindowLogIn(){
+        etUserName.setText("");
+        etPassword.setText("");
+    }
+
+    private void clearWindowRegister(){
+        etRegisterName.setText("");
+        etRegisterPassword.setText("");
+        etRegisterEmail.setText("");
+        chbEmployee.setChecked(false);
+        chbManager.setChecked(false);
     }
 }
